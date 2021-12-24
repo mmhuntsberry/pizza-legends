60df80c2b34c4af8008c51c3370547407be3f106
matt.huntsberry <matt.huntsberry@nielsen.com>
2021-12-24 13:50:26 -0800

    Overworld maps

--- Overworld.js
+++ Overworld.js
@@ -3,31 +3,34 @@ class Overworld {
     this.element = config.element;
     this.canvas = this.element.querySelector(".game-canvas");
     this.ctx = this.canvas.getContext("2d");
+    this.map = null;
+  }
+
+  startGameLoop() {
+    const step = () => {
+      // Clear canvas
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      // Draw lower layer
+      this.map.drawLowerImage(this.ctx);
+
+      // Draw Game Objects
+      Object.values(this.map.gameObjects).forEach((object) => {
+        object.sprite.draw(this.ctx);
+      });
+      // Draw upper layer
+      this.map.drawUpperImage(this.ctx);
+
+      requestAnimationFrame(() => {
+        step();
+      });
+    };
+    step();
   }
 
   init() {
     console.log("Hello from the overworld", this);
-    const image = new Image();
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
-    };
-    image.src = "/images/maps/DemoLower.png";
-
-    // Place some Game Objects
-    const hero = new GameObject({
-      x: 5,
-      y: 6,
-    });
-
-    const npc1 = new GameObject({
-      x: 7,
-      y: 8,
-      src: "/images/characters/people/npc1.png",
-    });
-
-    setTimeout(() => {
-      hero.sprite.draw(this.ctx);
-      npc1.sprite.draw(this.ctx);
-    }, 200);
+    this.map = new OverworldMap(window.OverworldMaps.Kitchen);
+    this.startGameLoop();
   }
 }
